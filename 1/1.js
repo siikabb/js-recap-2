bac7589bf208e18118f2b1038266ff1eb740f056
Joona Loukkola <111729194+siikabb@users.noreply.github.com>
2023-08-24 14:37:58 +0300

    excercise 1 done

---
+++ 1/1.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const fruits = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
+
+console.log(fruits);
+
+console.log(fruits.length);
+console.log(fruits[2]);
+console.log(fruits[fruits.length]);
+
+const vegetables = [];
+
+for (let i = 0; i <= 2; i++) {
+  vegetables.push(prompt('Enter a vegetable'));
+}
+
+console.log(vegetables);
+console.log(vegetables.length);
